a0b7d741251fc4162f15f64f743fdf2c6f59f4de
Hafiz Muhammad Junaid <junaid.waqar92@gmail.com>
2025-07-15 00:19:40 +0500

    - Initial Commit

---
+++ AlaramApp/AlaramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlaramAppApp.swift
+//  AlaramApp
+//
+//  Created by Hafiz Muhammad Junaid on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlaramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlaramApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlaramApp
+//
+//  Created by Hafiz Muhammad Junaid on 14/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
