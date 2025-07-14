538134170c74eeb8a98bc28cdec39482739274b6
Hafiz Muhammad Junaid <junaid.waqar92@gmail.com>
2025-07-15 00:48:20 +0500

    - launch screen added

---
+++ AlaramApp/Modules/Launch/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  AlaramApp
+//
+//  Created by Hafiz Muhammad Junaid on 15/07/2025.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Alaram App")
+    }
+}
+
+#Preview {
+    LaunchView()
+}
