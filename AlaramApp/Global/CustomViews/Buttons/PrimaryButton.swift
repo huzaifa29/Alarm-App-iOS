877ac88477168d379a7cff7974d63c78f5b31b5b
Hafiz Muhammad Junaid <junaid.waqar92@gmail.com>
2025-07-22 00:38:27 +0500

    - remove unused code

--- AlaramApp/Global/CustomViews/Buttons/PrimaryButton.swift
+++ AlaramApp/Global/CustomViews/Buttons/PrimaryButton.swift
@@ -27,7 +27,7 @@ struct PrimaryButton: View {
                             .blur(radius: 15.3)
                             .offset(y: 10)
                     )
-                    .frame(height: 50)
+                    .frame(height: 55)
                     .frame(maxWidth: .infinity)
 
                 // Main capsule background and borders
@@ -79,61 +79,6 @@ struct PrimaryButton: View {
             }
         }
     }
-    
-//    var body: some View {
-//        ZStack {
-//            // Glow behind the button (like Figma's drop shadow)
-//            Capsule()
-//                .fill(Color(.customF5C1FF).opacity(0.40))
-//                .frame(height: 80)
-//                .blur(radius: 13) // half of Figma blur radius for more natural look
-//            
-//            // Main button
-//            Button {
-//                action?()
-//            } label: {
-//                HStack(spacing: iconSpacing) {
-//                    if let leftIcon = leftIcon {
-//                        Image(leftIcon) // Replace with actual asset name
-//                            .resizable()
-//                            .frame(width: 30, height: 30)
-//                    }
-//                    
-//                    Text(text)
-//                        .font(Font.getFont(.bold, size: 18))
-//                        .foregroundStyle(Color(.custom2D2D40))
-//                }
-//                .frame(height: 55)
-//                .frame(maxWidth: .infinity)
-//                .padding(.horizontal, 24)
-//                .background(
-//                    LinearGradient(
-//                        gradient: Gradient(colors: [
-//                            Color(.customF4BDFF),
-//                            Color(.customF4BDFF).opacity(0.0)
-//                        ]),
-//                        startPoint: .top,
-//                        endPoint: .bottom
-//                    )
-//                )
-//                .overlay(
-//                    Capsule()
-//                        .strokeBorder(
-//                            LinearGradient(
-//                                colors: [
-//                                    Color(.customF6C5FE),
-//                                    Color.white
-//                                ],
-//                                startPoint: .top,
-//                                endPoint: .bottom
-//                            ),
-//                            lineWidth: 1
-//                        )
-//                )
-//                .clipShape(Capsule())
-//            }
-//        }
-//    }
 }
 
 
