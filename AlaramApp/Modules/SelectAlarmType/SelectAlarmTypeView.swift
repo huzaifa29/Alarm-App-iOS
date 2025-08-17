0014eda5661e346894ed5870a3c19dfc172b3c53
Hafiz Muhammad Junaid <junaid.waqar92@gmail.com>
2025-08-17 14:04:00 +0500

    - fix duplicate record issue

--- AlaramApp/Modules/SelectAlarmType/SelectAlarmTypeView.swift
+++ AlaramApp/Modules/SelectAlarmType/SelectAlarmTypeView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct SelectAlarmTypeView: View {
     @Binding var path: [HomeRoute]
     
-    @State var arrayAlarmType: [AlarmTypeItemView.DataModel] = []
+    @State var arrayAlarmType: [AlarmTypeItemView.DataModel] = [
+        .init(image: "ic_music", title: "Basic Alarm", desc: "You can choose music from library or can upload your favorite music in app library, set time to wake up and get tension free.", isSelected: false),
+        .init(image: "ic_microphone", title: "Customized voice alarm creation", desc: "Record your custom voice for peaceful morning, save your recording for later use add time and set alarm.", isSelected: false),
+        .init(image: "ic_message_text", title: "Text to speech alarm creation", desc: "Enter your text, select your favorite voice from character list and you can preview tune and set alarm for your special day.", isSelected: false)
+    ]
     @State var selectedIndex = 0
     
     var body: some View {
@@ -40,11 +44,6 @@ struct SelectAlarmTypeView: View {
             .padding(.top, 10)
             
             Spacer()
-        }
-        .onAppear {
-            arrayAlarmType.append(.init(image: "ic_music", title: "Basic Alarm", desc: "You can choose music from library or can upload your favorite music in app library, set time to wake up and get tension free.", isSelected: false))
-            arrayAlarmType.append(.init(image: "ic_microphone", title: "Customized voice alarm creation", desc: "Record your custom voice for peaceful morning, save your recording for later use add time and set alarm.", isSelected: false))
-            arrayAlarmType.append(.init(image: "ic_message_text", title: "Text to speech alarm creation", desc: "Enter your text, select your favorite voice from character list and you can preview tune and set alarm for your special day.", isSelected: false))
         }
         .navigationBarHidden(true)
     }
